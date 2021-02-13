87aca7fdbf9bfaedaac905f1ec9db5195874d565
Consuelo Sanna <consuelo.sanna@spindox.it>
2021-02-13 18:32:48 +0100

    created web build and changed webpack.config to PWA support

---
+++ web-build/precache-manifest.80c3b69c46a4da522058e11915806a11.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f80f812e46f94f3634b",
+    "url": "/static/js/app.924dcaad.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "4a3799bd54ca07d44aad",
+    "url": "/static/js/2.a8edc3bc.chunk.js"
+  },
+  {
+    "revision": "864b5a2a12e395a0aba167209a78df95",
+    "url": "/static/media/rock.79436ec1.png"
+  },
+  {
+    "revision": "cffb3100c0baea623b0353f9e933ef62",
+    "url": "/static/media/paper.599c25df.png"
+  },
+  {
+    "revision": "48d238a67d2b41996cd45194a93b7b10",
+    "url": "/static/media/scissors.f27b7752.png"
+  },
+  {
+    "revision": "8e327423d1a5e74202a76a3c4628f8fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a8edc3bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "50b1bd72d8b0c569ffcad83d622ec176",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "831b5927923a0e8ca7e017f0a0af5eab",
+    "url": "/manifest.json"
+  }
+];
