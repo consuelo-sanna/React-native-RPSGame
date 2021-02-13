5fe354f55fdf7d3e9f0778a18b025471b3777be8
Consuelo Sanna <consuelo.sanna@spindox.it>
2021-02-13 18:19:40 +0100

    create project

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View ,Button} from 'react-native';
+import PlayScreen from './src/components/PlayScreen'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [isPlay,setIsPlay] = useState(false);
+  return ( isPlay ?
+        <PlayScreen /> :
+        <View style={styles.container}>
+          <Text style={styles.header}>Rock Paper Scissors</Text>
+          <Text style={{fontSize:65}}>✊ ✋ ✌️ </Text>
+          <View style={styles.button}>
+            <Button style={styles.header} onPress={() => setIsPlay(!isPlay)} title="Play" /> 
+          </View>
+          <StatusBar style="auto" />
+        </View>
+
+   
   );
 }
 
@@ -18,4 +27,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontSize: 30,
+    marginBottom: 10,
+  },
+  button: {
+    marginTop: 30,
+  }
 });
---
+++ src/components/PlayScreen.js
@@ -0,0 +1,73 @@
+import React,{useState, useEffect } from 'react';
+import { StyleSheet, Text, View ,Button,Image } from 'react-native'
+
+const rock = require('../images/signs/rock.png')
+const paper = require('../images/signs/paper.png')
+const scissors = require('../images/signs/scissors.png')
+
+const bgColors = ['#1abc9c', '#3498db', '#9b59b6'];
+
+const randomImg = () => {
+  const emojis = [rock,paper,scissors];
+  const random = Math.floor((Math.random()*emojis.length));
+  return emojis[random]
+}
+
+const PlayScreen = () => {
+  const [counter, setCounter] = useState(3);
+
+  useEffect(()=> {
+      if(counter === 0) return;
+      const timer = setTimeout(()=>{
+          setCounter(counter - 1)//(prv => {console.log("prv ",prv); return prv-1} )
+      },500)
+
+      return () => clearTimeout(timer)
+
+  },[counter])
+
+
+  return (
+    <View style={StyleSheet.compose(
+      { backgroundColor: bgColors[counter - 1]},
+      styles.container)}
+    >
+      {(counter === 0) ?
+        (
+          <>
+            <Image source={randomImg()} style={styles.sign} />
+            <View style={styles.button}>
+              <Button  onPress={() => setCounter(3)} title="Play" />
+            </View>
+          </>
+        ) : (
+              <Text style={styles.counter}> {counter} </Text>
+        )
+      }
+        
+        
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  counter: {
+    fontSize: 150,
+    color: 'white',
+  },
+  sign: {
+    width: 300,
+    height: 300,
+  },
+  button: {
+    position: "absolute",
+    bottom: 40
+  }
+});
+
+export default PlayScreen;
